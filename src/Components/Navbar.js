8533ef37692a33bf6e2195f3d43942a77e4330e1
OnniRaisanen <t1raon00@students.oamk.fi>
2022-10-28 17:50:49 +0300

    yes

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function Navbar() {
+    return (
+        <ul class="nav">
+        <li class="nav-item"><a href="/#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="/About" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="/ContactUs" class="nav-link">ContactUs</a></li>
+      </ul>
+        
+    )
+}
